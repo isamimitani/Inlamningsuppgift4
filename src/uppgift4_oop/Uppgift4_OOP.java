736aae056337429f12c03a712e8bbd16688417e9
isami <isamimitani@live.se>
2018-11-14 10:57:56 +0100

    First commit

---
+++ src/uppgift4_oop/Uppgift4_OOP.java
@@ -0,0 +1,16 @@
+package uppgift4_oop;
+
+/**
+ *
+ * @author 
+ */
+public class Uppgift4_OOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
