792c11541f85e1de53e15ae31fc2ac3d615897f2
Hussein <Hussein@172.20.201.182>
2018-11-15 13:23:09 +0100

    Frågorna

--- src/uppgift4_oop/Quiz.java
+++ src/uppgift4_oop/Quiz.java
@@ -1,9 +1,16 @@
 package uppgift4_oop;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author isami
  */
 public class Quiz {
-    
+        
+   
 }
