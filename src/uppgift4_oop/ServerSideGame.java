15024a87ef1076773729dc59c9054c8dfc43cd88
isami <isamimitani@live.se>
2018-11-16 10:21:27 +0100

    Added ArrayList for categories and setter method. Added setCurrentPlayer method.

--- src/uppgift4_oop/ServerSideGame.java
+++ src/uppgift4_oop/ServerSideGame.java
@@ -1,10 +1,53 @@
 package uppgift4_oop;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
- * @author isami
+ * @author 
  */
 public class ServerSideGame {
     
-    ServerSidePlayer currentPlayer; 
+    private ServerSidePlayer currentPlayer; 
+    private ArrayList<String> categories = new ArrayList<>();
+    private ArrayList<Quiz> questions = new ArrayList<>();
+    
+//    public void createCategories(){
+//        for(int i=0; i<questions.size(); i++){
+//            String s = questions.get(i).getCategory();
+//            if(!categories.contains(s)){
+//                categories.add(s);
+//            }
+//        }
+//    }
+//    
+//    public Quiz getRandomQuiz(){
+//        Quiz quiz = null;
+//        Random rand = new Random();
+//        quiz = list.get(rand.nextInt(questions.size()));
+//        return quiz;
+//    }
+//    
+//    public Quiz getRandomQuiz(String category){
+//        Quiz quiz = null;
+//        ArrayList<Quiz> list = new ArrayList<>();
+//        for(Quiz q : questions){
+//            if(q.getCategory().equals(category)){
+//                list.add(q);
+//            }
+//        }
+//        Random rand = new Random();
+//        quiz = list.get(rand.nextInt(list.size()));
+//        return quiz;
+//    }
+    
+    public List<String> getCategories(){
+        return categories;
+    }
+    
+    public void setCurrentPlayer(ServerSidePlayer player){
+        currentPlayer = player;
+    }
 }
